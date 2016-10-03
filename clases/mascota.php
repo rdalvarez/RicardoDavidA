c6046f96146c6f7e06de15d94d934d746febe16f
rdalvarez <david_de_odin@hotmail.com>
2016-10-03 19:26:03 -0300

    listo clase mascota

--- clases/mascota.php
+++ clases/mascota.php
@@ -7,35 +7,45 @@ class Mascota
 	private $nombre;
  	private $edad;
   	private $fechaDeNacimiento;
-  	private $mascota;
-  	private $sexo;
-
+  	
 //--------------------------------------------------------------------------------//
 
 //--------------------------------------------------------------------------------//
 //--GETTERS Y SETTERS
-	public function GetNombre(){ return $this->nombre;}
-	public function GetEdad(){ return $this->edad;}
-	public function GetFechaDeNacimiento(){return $this->$fechaDeNacimiento;}
-	public function GetMascota(){ return $this->mascota;}
-	public function GetSexo(){ return $this->sexo;}
+	public function GetCodBarra()
+	{
+		return $this->codBarra;
+	}
+	public function GetNombre()
+	{
+		return $this->nombre;
+	}
+	public function GetPathFoto()
+	{
+		return $this->pathFoto;
+	}
 
-	public function SetNombre($valor){$this->nombre = $valor;}
-	public function SetEdad($valor){$this->edad = $valor;}
-	public function SetFechaDeNacimiento($valor){$this->$fechaDeNacimiento = $valor;}
-	public function SetMascota($valor){$this->mascota = $valor;}
-	public function SetSexo($valor){$this->sexo = $valor;}
+	public function SetCodBarra($valor)
+	{
+		$this->codBarra = $valor;
+	}
+	public function SetNombre($valor)
+	{
+		$this->nombre = $valor;
+	}
+	public function SetPathFoto($valor)
+	{
+		$this->pathFoto = $valor;
+	}
 
 //--------------------------------------------------------------------------------//
 //--CONSTRUCTOR
-	public function __construct($nombre=NULL, $Edad=NULL, $fechaDeNacimiento=NULL, $mascota=NULL, $sexo=NULL)
+	public function __construct($codBarra=NULL, $nombre=NULL, $pathFoto=NULL)
 	{
-		if($nombre !== NULL && $edad !== NULL && $fechaDeNacimiento!==NULL){
+		if($codBarra !== NULL && $nombre !== NULL){
+			$this->codBarra = $codBarra;
 			$this->nombre = $nombre;
-			$this->edad = $edad;
-			$this->fechaDeNacimiento = $fechaDeNacimiento;
-			$this->mascota = $mascota;
-			$this->sexo = $sexo;
+			$this->pathFoto = $pathFoto;
 		}
 	}
 
@@ -43,7 +53,7 @@ class Mascota
 //--TOSTRING	
   	public function ToString()
 	{
-	  	return $this->nombre." - ".$this->edad." - ".$this->fechaDeNacimiento." - ".$this->mascota." - ".$this->sexo."\r\n";
+	  	return $this->codBarra." - ".$this->nombre." - ".$this->pathFoto."\r\n";
 	}
 //--------------------------------------------------------------------------------//
 
@@ -54,7 +64,7 @@ class Mascota
 		$resultado = FALSE;
 		
 		//ABRO EL ARCHIVO
-		$ar = fopen("DB/mascotas.txt", "a");
+		$ar = fopen("archivos/productos.txt", "a");
 		
 		//ESCRIBO EN EL ARCHIVO
 		$cant = fwrite($ar, $obj->ToString());
@@ -68,55 +78,55 @@ class Mascota
 		
 		return $resultado;
 	}
-	public static function TraerTodasLasMascotas()
+	public static function TraerTodosLosProductos()
 	{
 
-		$ListaDeMascotasLeidas = array();
+		$ListaDeProductosLeidos = array();
 
 		//leo todos los productos del archivo
-		$archivo=fopen("DB/mascotas.txt", "r");
+		$archivo=fopen("archivos/productos.txt", "r");
 		
 		while(!feof($archivo))
 		{
 			$archAux = fgets($archivo);
 			$productos = explode(" - ", $archAux);
 			//http://www.w3schools.com/php/func_string_explode.asp
-			$mascotas[0] = trim($mascotas[0]);
-			if($mascotas[0] != ""){
-				$ListaDeMascotasLeidas[] = new Producto($mascotas[0], $mascotas[1],$mascotas[2]);
+			$productos[0] = trim($productos[0]);
+			if($productos[0] != ""){
+				$ListaDeProductosLeidos[] = new Producto($productos[0], $productos[1],$productos[2]);
 			}
 		}
 		fclose($archivo);
 		
-		return $ListaDeMascotasLeidas;
+		return $ListaDeProductosLeidos;
 		
 	}
 	public static function Modificar($obj)
 	{
 		$resultado = TRUE;
 		
-		$ListaDeMascotasLeidos = Mascota::TraerTodosLasMascotas();
-		$ListaDeMascotas = array();
+		$ListaDeProductosLeidos = Producto::TraerTodosLosProductos();
+		$ListaDeProductos = array();
 		$imagenParaBorrar = NULL;
 		
-		for($i=0; $i<count($ListaDeMascotasLeidos); $i++){
-			if($ListaDeMascotasLeidos[$i]->codBarra == $obj->codBarra){//encontre el modificado, lo excluyo
-				$imagenParaBorrar = trim($ListaDeMascotasLeidos[$i]->pathFoto);
+		for($i=0; $i<count($ListaDeProductosLeidos); $i++){
+			if($ListaDeProductosLeidos[$i]->codBarra == $obj->codBarra){//encontre el modificado, lo excluyo
+				$imagenParaBorrar = trim($ListaDeProductosLeidos[$i]->pathFoto);
 				continue;
 			}
-			$ListaDeMascotas[$i] = $ListaDeMascotasLeidos[$i];
+			$ListaDeProductos[$i] = $ListaDeProductosLeidos[$i];
 		}
 
-		array_push($ListaDeMascotas, $obj);//agrego el producto modificado
+		array_push($ListaDeProductos, $obj);//agrego el producto modificado
 		
 		//BORRO LA IMAGEN ANTERIOR
-		unlink("DB/".$imagenParaBorrar);
+		unlink("archivos/".$imagenParaBorrar);
 		
 		//ABRO EL ARCHIVO
-		$ar = fopen("DB/mascotas.txt", "w");
+		$ar = fopen("archivos/productos.txt", "w");
 		
 		//ESCRIBO EN EL ARCHIVO
-		foreach($ListaDeMascotas AS $item){
+		foreach($ListaDeProductos AS $item){
 			$cant = fwrite($ar, $item->ToString());
 			
 			if($cant < 1)
@@ -138,16 +148,16 @@ class Mascota
 			
 		$resultado = TRUE;
 		
-		$ListaDeMascotasLeidos = Producto::TraerTodosLosProductos();
-		$ListaDeMascotas = array();
+		$ListaDeProductosLeidos = Producto::TraerTodosLosProductos();
+		$ListaDeProductos = array();
 		$imagenParaBorrar = NULL;
 		
-		for($i=0; $i<count($ListaDeMascotasLeidos); $i++){
-			if($ListaDeMascotasLeidos[$i]->codBarra == $codBarra){//encontre el borrado, lo excluyo
-				$imagenParaBorrar = trim($ListaDeMascotasLeidos[$i]->pathFoto);
+		for($i=0; $i<count($ListaDeProductosLeidos); $i++){
+			if($ListaDeProductosLeidos[$i]->codBarra == $codBarra){//encontre el borrado, lo excluyo
+				$imagenParaBorrar = trim($ListaDeProductosLeidos[$i]->pathFoto);
 				continue;
 			}
-			$ListaDeMascotas[$i] = $ListaDeMascotasLeidos[$i];
+			$ListaDeProductos[$i] = $ListaDeProductosLeidos[$i];
 		}
 
 		//BORRO LA IMAGEN ANTERIOR
@@ -157,7 +167,7 @@ class Mascota
 		$ar = fopen("archivos/productos.txt", "w");
 		
 		//ESCRIBO EN EL ARCHIVO
-		foreach($ListaDeMascotas AS $item){
+		foreach($ListaDeProductos AS $item){
 			$cant = fwrite($ar, $item->ToString());
 			
 			if($cant < 1)
